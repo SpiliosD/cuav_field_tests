8f1a1d1fecd800b7564517e2e1f8fdabcd9a1019
Spilios <sdellis@raymetrics.com>
2025-11-20 14:32:11 +0200

    Pycharm run

--- data_reader/analysis/visualization.py
+++ data_reader/analysis/visualization.py
@@ -741,8 +741,8 @@ def create_profile_visualizations(
         ax.set_xlabel("Range (m)", fontsize=12)
         ax.set_ylabel("SNR", fontsize=12)
         ax.set_title("SNR Profiles (All Timestamps)", fontsize=14, fontweight="bold")
-        ax.set_xlim(0, 3000)
-        ax.set_ylim(1, 10)
+        ax.set_xlim(-200, 3000)
+        #ax.set_ylim(0, 65)
         ax.grid(True, alpha=0.3)
         
         if output_dir is not None:
@@ -768,7 +768,7 @@ def create_profile_visualizations(
         ax.set_ylabel("Wind Speed (m/s)", fontsize=12)
         ax.set_title("Wind Profiles (All Timestamps)", fontsize=14, fontweight="bold")
         ax.set_xlim(0, 3000)
-        ax.set_ylim(-10, 10)
+        #ax.set_ylim(-10, 10)
         ax.grid(True, alpha=0.3)
         
         if output_dir is not None:
--- sandbox/check_singe_pds.py
+++ sandbox/check_singe_pds.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 def main_test():
-    file = r'D:\Raymetrics_Tests\BOMA2025\20250926\Spectra\User\20250926\Wind\2025-09-26\09-26_07h\09-26_07-20\spectra_2025-09-26_07-20-00.65.txt'
+    file = r'D:\Raymetrics_Tests\BOMA2025\RawSpectra_txt\20250923\Spectra\User\20250923\Wind\2025-09-23\09-23_12h\09-23_12-50\spectra_2025-09-23_12-58-47.21.txt'
     data = np.loadtxt(file, delimiter='\t', skiprows=13)
     freq = np.arange(64) * 400/128
     for i, d in enumerate(data):
